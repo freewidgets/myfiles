8a5e7646c1e31d4bdee84a923ce27a7386eecd6c
freewidgets <eltonregs@gmail.com>
2017-08-12 17:40:00 +0800

    countdown
    countdown widget with server time

---
+++ cd/servertime.php
@@ -0,0 +1,7 @@
+ <?php
+date_default_timezone_set ('Canada/Eastern');
+ // $now = new DateTime();
+ // echo $now->format("Y/m/d H:i:s")."\n";
+ $now = new DateTime();
+ echo $now->format("M j, Y H:i:s O")."\n";
+ ?> 
