0b04a364d1fb01d3e037905e4d02d49615810c22
freewidgets <eltonregs@gmail.com>
2017-08-13 16:04:00 +0800

    timezone change

--- cd/servertime.php
+++ cd/servertime.php
@@ -1,5 +1,5 @@
  <?php
-date_default_timezone_set ('Canada/Eastern');
+date_default_timezone_set ('America/Toronto');
  // $now = new DateTime();
  // echo $now->format("Y/m/d H:i:s")."\n";
  $now = new DateTime();
